92d47166d1487ed046868148f8b770880b728a72
Matvey <morozovmatvej75@gmail.com>
2023-03-17 19:08:25 +0300

    implement authorization middleware

--- server/src/middleware/authorize.ts
+++ server/src/middleware/authorize.ts
@@ -1,7 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import { UnauthorizedErr } from "../utils/errs";
+import jwt from "jsonwebtoken";
 
 export default function authorize(req: Request, res: Response, next: NextFunction) {
-  console.log("This is authorization middleware speaking!");
-  console.log(req.headers.authorization);
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthorizedErr("invalid authorization header");
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    (req as any).user = jwt.verify(token, process.env.JWT_SECRET as string);
+  } catch (err) {
+    throw new UnauthorizedErr("invalid jwt signature");
+  }
   next();
 }
