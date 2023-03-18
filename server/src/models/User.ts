a8da6fad910b4822904bf05520c98d5241bb641a
Matvey <morozovmatvej75@gmail.com>
2023-03-18 17:22:35 +0300

    format User model & add profileId custom validator

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -8,72 +8,25 @@ import jwt from "jsonwebtoken";
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 const UserSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 30
-  },
-  lastName: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 30
-  },
-  email: {
-    type: String,
-    match: emailRegex,
-    maxlength: 255,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 10,
-    maxlength: 50
-  },
-  pictureUrl50px: {
-    type: String,
-    default: "assets/pictures/default.svg",
-    maxlength: 1000
-  },
-  pictureUrl100px: {
-    type: String,
-    default: "assets/pictures/default.svg",
-    maxlength: 1000
-  },
-  pictureUrl400px: {
-    type: String,
-    default: "assets/pictures/default.svg",
-    maxlength: 1000
-  },
-  profileId: {
-    type: String,
-    maxlength: 100,
-    required: true,
-    unique: true
-  },
-  friends: {
-    type: Array,
-    default: []
-  },
-  birthday: {
-    type: Date
-  },
-  address: {
-    type: String,
-    maxlength: 300
-  },
-  occupation: {
-    type: String,
-    maxlength: 100
-  },
-  status: {
-    type: String,
-    maxlength: 300 
-  },
+  firstName: { type: String, required: true, minlength: 3, maxlength: 30 },
+  lastName: { type: String, required: true, minlength: 3, maxlength: 30 },
+  email: { type: String, match: emailRegex, maxlength: 255, required: true, unique: true },
+  password: { type: String, required: true, minlength: 10, maxlength: 50 },
+  pictureUrl50px: { type: String, default: "assets/pictures/default.svg", maxlength: 1000 },
+  pictureUrl100px: { type: String, default: "assets/pictures/default.svg", maxlength: 1000 },
+  pictureUrl400px: { type: String, default: "assets/pictures/default.svg", maxlength: 1000 },
+  profileId: { type: String, maxlength: 30, required: true, unique: true, validate: profileIdValidator },
+  friends: { type: Array, default: [] },
+  birthday: { type: Date },
+  address: { type: String, maxlength: 300 },
+  occupation: { type: String, maxlength: 100 },
+  status: { type: String, maxlength: 300 },
 }, { timestamps: true })
+
+function profileIdValidator(val: string) {
+  const urlRegex = /^[a-z0-9-_]+$/i;
+  return urlRegex.test(val);
+}
 
 UserSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt();
