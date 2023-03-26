5ab19b57a627915d4f667dd867c9820f46c9da83
Matvey <morozovmatvej75@gmail.com>
2023-03-26 12:29:50 +0300

    delete all posts made by user when account is deleted

--- server/src/controllers/account.ts
+++ server/src/controllers/account.ts
@@ -1,13 +1,7 @@
-import { ObjectId } from "mongoose";
+import Post from "../models/Post";
 import User from "../models/User";
 import { NotFoundErr } from "../utils/errs";
 import { IReq, IRes } from "../utils/ReqResInterfaces";
-
-export async function deleteAccount(req: IReq, res: IRes) {
-  const deletedAccount = await User.findByIdAndDelete(req.data.user.userId);
-  if (!deletedAccount) throw new NotFoundErr("account not found");
-  res.status(200).json(deletedAccount);
-}
 
 export async function patchAccount(req: IReq, res: IRes) {
   const patchedAccount = await User.findByIdAndUpdate(
@@ -17,6 +11,13 @@ export async function patchAccount(req: IReq, res: IRes) {
   )
   if (!patchedAccount) throw new NotFoundErr("account not found");
   res.status(200).json(patchedAccount);
+}
+
+export async function deleteAccount(req: IReq, res: IRes) {
+  const deletedAccount = await User.findByIdAndDelete(req.data.user.userId);
+  if (!deletedAccount) throw new NotFoundErr("account not found");
+  await Post.deleteMany({ _id: { $in: deletedAccount.posts }});
+  res.status(200).json(deletedAccount);
 }
 
 export async function addFriend(req: IReq, res: IRes) {
