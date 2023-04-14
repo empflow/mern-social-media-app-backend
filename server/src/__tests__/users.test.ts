8a9a1190ec0af4cd33114941654357a329652b56
Matvey <morozovmatvej75@gmail.com>
2023-04-14 20:40:51 +0300

    thank goodness the first test seems to work

--- server/src/__tests__/users.test.ts
+++ server/src/__tests__/users.test.ts
@@ -1,53 +1,43 @@
 import requests from "supertest";
-import createServer from "../utils/createServer";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import createServer from "../utils/createServer";
 
 const app = createServer();
 
-const allSignUpData = {
-  firstName: "John",
-  lastName: "Doe",
-  password: "1234567890",
-  email: "johndoe@gmail.com"
-}
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDJkYzRiNTFiYjNhN2MxYWJhZDMzMDQiLCJpYXQiOjE2ODA3MjE4MDAsImV4cCI6MTY4MzMxMzgwMH0.fz4ErCIfb_bZiRcn5ZLoDADbx6RClaVr881QtpfQkVA";
-const authHeader = `Bearer ${token}`;
-
-beforeEach(async () => {
+beforeAll(async () => {
   const mongod = await MongoMemoryServer.create();
-  await mongoose.connect(mongod.getUri());
+  mongoose.connect(mongod.getUri());
 })
 
-afterEach(async () => {
+afterAll(async () => {
   await mongoose.disconnect();
   await mongoose.connection.close();
 })
 
 describe("auth", () => {
-  describe("sign-up", () => {
-    describe("given all sign up data is present", () => {
-      it("returns the newly created user and token and does not include the password", async () => {
-        const { statusCode, body } = await requests(app)
+  describe("sing-up", () => {
+    describe("given all correct sign-up data", () => {
+      it("returns created user and token", () => {
+        return requests(app)
           .post("/auth/sign-up")
-          .send(allSignUpData);
+          .send({
+            firstName: "john",
+            lastName: "doe",
+            email: "johndoe@gmail.com",
+            password: "1234567890"
+          })
+          .then(res => {
+            const { body, statusCode, headers } = res;
+            console.log(body);
+            console.log(headers);
 
-        expect(statusCode).toBe(201);
-        expect(body).toHaveProperty("user");
-        expect(body).toHaveProperty("token");
-        expect(body.user).not.toHaveProperty("password");
+            expect(headers["content-type"]).toMatch(/json/);
+            expect(statusCode).toBe(201);
+            expect(body.user).toBeDefined();
+            expect(body.token).toBeDefined();
+          })
       })
     })
-
-    // i fucking hate this shit never works no matter what i fucking do i cant take this any fucking more
-    // describe("given the first name is missing", () => {
-    //   test("returns a 400 error", () => {
-    //     requests(app)
-    //       .post("/auth/sign-up")
-    //       .send({ ...allSignUpData, firstName: undefined })
-    //       .catch(err => expect)
-
-    //   }, 10000)
-    // })
   })
 })
