b2297e91ba30d31500b64c73ce0762190d1fc75e
Matvey <morozovmatvej75@gmail.com>
2023-04-14 21:06:06 +0300

    add duplicate user test (needs some more work)

--- server/src/__tests__/users.test.ts
+++ server/src/__tests__/users.test.ts
@@ -5,38 +5,48 @@ import createServer from "../utils/createServer";
 
 const app = createServer();
 
-beforeAll(async () => {
-  const mongod = await MongoMemoryServer.create();
+let mongod: MongoMemoryServer;
+beforeEach(async () => {
+  mongod = await MongoMemoryServer.create();
   mongoose.connect(mongod.getUri());
-})
+});
 
-afterAll(async () => {
+afterEach(async () => {
   await mongoose.disconnect();
   await mongoose.connection.close();
-})
+  await mongod.stop();
+});
+
+const signUpData = {
+  firstName: "john",
+  lastName: "doe",
+  email: "johndoe@gmail.com",
+  password: "1234567890"
+};
 
 describe("auth", () => {
   describe("sing-up", () => {
     describe("given all correct sign-up data", () => {
-      it("returns created user and token", () => {
-        return requests(app)
+      it("returns created user and token", async () => {
+        const { body, statusCode, headers } = await requests(app)
           .post("/auth/sign-up")
-          .send({
-            firstName: "john",
-            lastName: "doe",
-            email: "johndoe@gmail.com",
-            password: "1234567890"
-          })
-          .then(res => {
-            const { body, statusCode, headers } = res;
-            console.log(body);
-            console.log(headers);
+          .send(signUpData);
 
-            expect(headers["content-type"]).toMatch(/json/);
-            expect(statusCode).toBe(201);
-            expect(body.user).toBeDefined();
-            expect(body.token).toBeDefined();
-          })
+          expect(headers["content-type"]).toMatch(/json/);
+          expect(statusCode).toBe(201);
+          expect(body.user).toBeDefined();
+          expect(body.token).toBeDefined();
+      })
+    })
+
+    describe("given user with this email already exists", () => {
+      it("returns a duplicate error", async () => {
+        // TODO: create another user to trigger the error (tests run on different DB instances in different threads)
+        const { body, statusCode, headers } = await requests(app)
+        .post("/auth/sign-up")
+        .send(signUpData);
+
+        expect(statusCode).toBe(201);
       })
     })
   })
