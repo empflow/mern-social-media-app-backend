5e63f2aa9f77db728997f5be7f079dad33183b73
Matvey <morozovmatvej75@gmail.com>
2023-04-19 18:29:09 +0300

    fix signUpBeforeEachSignIn cb

--- server/src/__tests__/test_auth/signUpBeforeEachSignIn.ts
+++ server/src/__tests__/test_auth/signUpBeforeEachSignIn.ts
@@ -1,11 +1,9 @@
 import requests from "supertest";
 import app from "../../app";
 import assertJson from "../utils/assertJson";
-import getSignUpData from "./getSignUpData";
+import { signUpData } from "./auth.test";
 
 export default async function signUpBeforeEachSignIn() {
-  const signUpData = getSignUpData();
-
   const { headers } = await requests(app)
     .post("/auth/sign-up")
     .send(signUpData);
