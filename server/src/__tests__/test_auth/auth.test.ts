49d5553783a4268eb9f80b1a08a39896e961700e
Matvey <morozovmatvej75@gmail.com>
2023-04-19 18:28:45 +0300

    add all previous tests back (working, verified)

--- server/src/__tests__/test_auth/auth.test.ts
+++ server/src/__tests__/test_auth/auth.test.ts
@@ -7,9 +7,16 @@ import app from "../../app";
 import getSignUpData from "./getSignUpData";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import testMissingSignUpData from "./testMissingSignUpData";
+import { getRandomProfilePath } from "../../utils/pathsGenerators";
+import convertSignUpDataToSignInData from "./convertSignUpDataToSignInData";
+import getMongoUri from "../../utils/getMongoUri";
+import assertJson from "../utils/assertJson";
+import User from "../../models/User";
+import testSignUpFieldIsOfLength from "./testSignUpFieldIsOfLength";
+import signUpBeforeEachSignIn from "./signUpBeforeEachSignIn";
+import getStrOfLength from "../../utils/getStrOfLength";
+import testMissingSignInData from "./testMissingSignInData";
 
-
-const signUpData = getSignUpData();
 
 let mongod: MongoMemoryServer;
 
@@ -24,26 +31,42 @@ afterEach(async () => {
   await mongoose.connection.close();
 })
 
-describe("auth", () => {
+export const signUpData = getSignUpData();
+export const signInData = convertSignUpDataToSignInData(signUpData);
 
-  describe("sign-up", () => {
+export const userDataForModel = {
+  ...getSignUpData(),
+  profilePath: getRandomProfilePath()
+}
+
+describe("auth", () => {
+  describe("sing-up", () => {
     describe("given all correct sign-up data", () => {
-      it("returns 201 and created user", async () => {
+      it("returns 201 created user and token (no password)", async () => {
         const { body, statusCode, headers } = await requests(app)
           .post("/auth/sign-up")
           .send(signUpData);
 
-        expect(statusCode).toBe(201);
+          assertJson(headers);
+          expect(statusCode).toBe(201);
+          expect(body.user).toBeDefined();
+          expect(body.token).toBeDefined();
+          expect(body.password).toBeUndefined();
       })
     })
 
-    describe("not given firstName", () => {
-      it("returns 400 bad request error", async () => {
+    describe("given user with this email already exists", () => {
+      it("returns a duplicate error", async () => {
+        await User.create(userDataForModel);
+        
+        const signUpData = { ...userDataForModel, profilePath: undefined };
         const { body, statusCode, headers } = await requests(app)
           .post("/auth/sign-up")
-          .send({ ...signUpData });
+          .send(signUpData);
 
-        expect(statusCode).toBe(201);
+        assertJson(headers);
+        expect(body.message).toBeDefined();
+        expect(statusCode).toBe(409);
       })
     })
 
@@ -51,5 +74,100 @@ describe("auth", () => {
     testMissingSignUpData("lastName");
     testMissingSignUpData("email");
     testMissingSignUpData("password");
+
+    testSignUpFieldIsOfLength("firstName", 30);
+    testSignUpFieldIsOfLength("firstName", 29);
+    testSignUpFieldIsOfLength("firstName", 31);
+    testSignUpFieldIsOfLength("firstName", 3);
+    testSignUpFieldIsOfLength("firstName", 2);
+
+    testSignUpFieldIsOfLength("lastName", 30);
+    testSignUpFieldIsOfLength("lastName", 29);
+    testSignUpFieldIsOfLength("lastName", 31);
+    testSignUpFieldIsOfLength("lastName", 3);
+    testSignUpFieldIsOfLength("lastName", 2);
+
+    testSignUpFieldIsOfLength("email", 255);
+    testSignUpFieldIsOfLength("email", 6);
+    testSignUpFieldIsOfLength("email", 254);
+    testSignUpFieldIsOfLength("email", 7);
+    testSignUpFieldIsOfLength("email", 8);
+
+    testSignUpFieldIsOfLength("password", 9);
+    testSignUpFieldIsOfLength("password", 101);
+    testSignUpFieldIsOfLength("password", 10);
+    testSignUpFieldIsOfLength("password", 100);
+    testSignUpFieldIsOfLength("password", 50);
+
+    describe("create user with invalid profile path", () => {
+      it("returns 400 BadRequest error", async () => {
+        const invalidProfilePath = "$hello#";
+
+        await expect(User.create({ ...userDataForModel, profilePath: invalidProfilePath }))
+          .rejects.toThrow();
+      })
+    })
+
+    describe("create user with profilePath that's too long (31 chars)", () => {
+      it("throws an error", async () => {
+        const profilePath = getStrOfLength(31);
+        await expect(User.create({ ...userDataForModel, profilePath }))
+          .rejects.toThrow();
+      })
+    })
+
+    describe("create user with profilePath that's too short (2 chars)", () => {
+      it("throws an error", async () => {
+        const profilePath = getStrOfLength(2);
+        await expect(User.create({ ...userDataForModel, profilePath }))
+          .rejects.toThrow();
+      })
+    })
+  })
+
+  describe("sign-in", () => {
+    beforeEach(signUpBeforeEachSignIn);
+
+    describe("given all correct sign-in data", () => {
+      it("returns 200 and token", async () => {
+        const { body, statusCode, headers } = await requests(app)
+          .post("/auth/sign-in")
+          .send(signInData)
+
+        assertJson(headers);
+        expect(statusCode).toBe(200);
+        expect(body.token).toBeDefined();
+        expect(body.user).toBeDefined();
+        expect(body.user._id).toBeDefined();
+        expect(body.user.profilePath).toBeDefined();
+      })
+    })
+
+    describe("user doesn't exist (no user with such email)", () => {
+      it("returns 404 not found", async () => {
+        const { body, statusCode, headers } = await requests(app)
+          .post("/auth/sign-in")
+          .send({ ...signInData, email: "thisDoesntExist@gmail.com" });
+
+        assertJson(headers);
+        expect(statusCode).toBe(404);
+        expect(body.message).toBe("user not found");
+      })
+    })
+
+    describe("wrong password", () => {
+      it("returns 401 unauthorized", async () => {
+        const { body, statusCode, headers } = await requests(app)
+          .post("/auth/sign-in")
+          .send({ ...signInData, password: "wrong-password" });
+
+        assertJson(headers);
+        expect(body.message).toBe("wrong password");
+        expect(statusCode).toBe(401);
+      })
+    })
+
+    testMissingSignInData("email");
+    testMissingSignInData("password");
   })
 })
