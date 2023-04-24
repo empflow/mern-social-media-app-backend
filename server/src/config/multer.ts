08752b8bafcfb346840bc1177b5e7963dd789d9f
Matvey <morozovmatvej75@gmail.com>
2023-04-24 19:47:49 +0300

    allow more file extensions

--- server/src/config/multer.ts
+++ server/src/config/multer.ts
@@ -1,11 +1,12 @@
 import { Options } from "multer";
 import path from "node:path";
 
-const allowedFileExts = [".png", ".svg", ".jpg", ".jpeg", ".heic"]
+const allowedFileExts = [".png", ".svg", ".jpg", ".jpeg", ".heic", ".webp", ".avif", ".jfif", ".pjpeg", ".pjp", ".apng"]
 
 const multerOptions: Options = {
   fileFilter(req, file, callback) {
     const fileExt = path.extname(file.originalname);
+
     if (allowedFileExts.includes(fileExt)) {
       callback(null, true);
     } else {
