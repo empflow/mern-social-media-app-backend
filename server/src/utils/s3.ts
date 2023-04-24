e95aa159c4226d06c51e49d74f151f006fe22766
Matvey <morozovmatvej75@gmail.com>
2023-04-24 18:44:08 +0300

    add s3 initilizer file

---
+++ server/src/utils/s3.ts
@@ -0,0 +1,21 @@
+import S3 from "aws-sdk/clients/s3";
+import getEnvVar from "./getEnvVar";
+
+export const bucketName = getEnvVar("S3_BUCKET_NAME");
+export const region = getEnvVar("S3_REGION");
+export const endpoint = getEnvVar("S3_ENDPOINT");
+const accessKeyId = getEnvVar("S3_ACCESS_KEY_ID");
+const secretAccessKey = getEnvVar("S3_SECRET_ACCESS_KEY");
+
+const s3 = new S3({
+  credentials: {
+    accessKeyId,
+    secretAccessKey
+  },
+  region,
+  endpoint,
+  apiVersion: "latest",
+  s3ForcePathStyle: true
+})
+
+export default s3;
