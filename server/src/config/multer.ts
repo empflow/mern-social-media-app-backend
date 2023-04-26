959620de2175aa01046e5c5b12dba8e0bcff670a
Matvey <morozovmatvej75@gmail.com>
2023-04-26 17:31:28 +0300

    remove .heic from list of allowed exts & improve err message on forbidden ext

--- server/src/config/multer.ts
+++ server/src/config/multer.ts
@@ -1,19 +1,31 @@
 import { Options } from "multer";
 import path from "node:path";
 
-const allowedFileExts = [".png", ".svg", ".jpg", ".jpeg", ".heic", ".webp", ".avif", ".jfif", ".pjpeg", ".pjp", ".apng"]
+const allowedFileExts = [".png", ".svg", ".jpg", ".jpeg", ".webp", ".avif", ".jfif", ".pjpeg", ".pjp", ".apng"]
 
 const multerOptions: Options = {
   fileFilter(req, file, callback) {
-    console.log(file);
     const fileExt = path.extname(file.originalname);
+
     if (allowedFileExts.includes(fileExt)) {
       callback(null, true);
     } else {
-      const err = new Error(`Forbidden file extension. You can only use ${allowedFileExts} extensions`);
+      const formattedAllowedFileExts = getFormattedAllowedFileExts();
+      const err = new Error(
+        `Forbidden file extension. You can only use ${formattedAllowedFileExts} extensions`
+      );
       callback(err);
     }
   }
+}
+
+function getFormattedAllowedFileExts() {
+  let result = "";
+  allowedFileExts.forEach((ext, i) => (
+    result += `${ext}${allowedFileExts.length - 1 === i ? "" : " "}`
+  ));
+
+  return result;
 }
 
 export default multerOptions;
