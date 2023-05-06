00a759b07ed0c41713682c9e42dab29ae065cf1f
Matvey <morozovmatvej75@gmail.com>
2023-05-06 15:04:05 +0300

    improve signUp() code by moving more logic into fns

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -9,20 +9,10 @@ import { optimizeImgAndUploadIn4Sizes } from "../utils/s3";
 export async function signUp(req: IReq, res: IRes) {
   // req.body contains other properties like `firstName` and `lastName` as well
   const { password, email } = req.body;
-  
-  if (!password) throw new BadRequestErr("password is required");
-  if (password.length < 10 || password.length > 100) {
-    throw new BadRequestErr("password must be at least 10 and not over 100 characters long");
-  }
-  
-  const userWithSameEmail = await User.findOne({ email });
-  if (userWithSameEmail) throw new ConflictErr("user with this email already exists");
 
-  if (req.file) {
-    const { buffer } = req.file;
-    const uploadResult = await optimizeImgAndUploadIn4Sizes(buffer);
-    console.log(uploadResult);
-  }
+  validatePassword(password);
+  checkForEmailConflict(email);  
+  await uploadAvatarIfPresent(req.file);
 
   const profilePath = getRandomProfilePath();
   const user = await User.create({ ...req.body, profilePath });
@@ -36,9 +26,26 @@ export async function signIn(req: IReq, res: IRes) {
   const token = await user.createJwt();
   res.status(200).json({
     token,
-    user: {
-      _id: user._id,
-      profilePath: user.profilePath
-    }
+    user: { _id: user._id, profilePath: user.profilePath }
   });
+}
+
+function validatePassword(password: string | undefined) {
+  if (!password) throw new BadRequestErr("password is required");
+  if (password.length < 10 || password.length > 100) {
+    throw new BadRequestErr("password must be at least 10 and not over 100 characters long");
+  }
+}
+
+async function checkForEmailConflict(email: string) {
+  const userWithSameEmail = await User.findOne({ email });
+  if (userWithSameEmail) throw new ConflictErr("user with this email already exists");
+}
+
+async function uploadAvatarIfPresent(file: Express.Multer.File | undefined) {
+  if (file) {
+    const { buffer } = file;
+    const uploadResult = await optimizeImgAndUploadIn4Sizes(buffer);
+    console.log(uploadResult);
+  }
 }
