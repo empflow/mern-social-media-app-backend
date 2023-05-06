360f4178bdfeade55d9f916acade101964dd8ed7
Matvey <morozovmatvej75@gmail.com>
2023-05-06 15:52:34 +0300

    fix user & friend projections & make them typed

--- server/src/utils/projections/friendProjection.ts
+++ server/src/utils/projections/friendProjection.ts
@@ -1,8 +1,11 @@
-const friendProjection = {
+import { IUser } from "../../models/User";
+
+const friendProjection: Partial<Record<keyof IUser, number>> = {
   firstName: 1,
   lastName: 1,
-  pictureUrl50px: 1,
-  pictureUrl100px: 1,
+  avatarUrl400px: 1,
+  avatarUrl100px: 1,
+  avatarUrl50px: 1,
   profilePath: 1
 }
 
--- server/src/utils/projections/userProjection.ts
+++ server/src/utils/projections/userProjection.ts
@@ -1,12 +1,19 @@
-const userProjection = {
+import { IUser } from "../../models/User";
+
+const userProjection: Partial<Record<keyof IUser, number>> = {
   firstName: 1,
   lastName: 1,
-  email: 1,
-  pictureUrl50px: 1,
-  pictureUrl100px: 1,
-  pictureUrl400px: 1,
+  avatarUrl400px: 1,
+  avatarUrl100px: 1,
+  avatarUrl50px: 1,
   profilePath: 1,
-  friends: 1
+  friends: 1,
+  occupation: 1,
+  birthday: 1,
+  canAnyonePost: 1,
+  city: 1,
+  status: 1,
+  createdAt: 1,
 }
 
 export default userProjection;
