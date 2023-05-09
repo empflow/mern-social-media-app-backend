c43577b9303ab3998894f35dc94398fbb0d28ba7
Matvey <morozovmatvej75@gmail.com>
2023-05-09 15:24:57 +0300

    update posts tests to use givenNImgsAndTextContent() helper

--- server/src/__tests__/test_posts/posts.test.ts
+++ server/src/__tests__/test_posts/posts.test.ts
@@ -12,16 +12,18 @@ import getUserDataForModel from "../utils/getUserDataForModel";
 import { HydratedDocument } from "mongoose";
 import path from "node:path";
 import { IImgUploadResult } from "../../utils/optimizeAndUploadPostImgs";
+import expectImgsUrlsMatchHttps from "./expectImgsUrlsMatchHttps";
+import givenNImgsAndTextContent from "./givenNImgsAndTextContent/givenNImgsAndTextContent";
 
 
-let authHeader1: string;
-let authHeader2: string;
-let user1: HydratedDocument<IUser>;
-let user2: HydratedDocument<IUser>;
+export let authHeader1: string;
+export let authHeader2: string;
+export let user1: HydratedDocument<IUser>;
+export let user2: HydratedDocument<IUser>;
 
 let mongod: MongoMemoryServer;
 
-const jpegImgPath = path.join(__dirname, "../data/avatar.jpeg");
+export const jpegImgPath = path.join(__dirname, "../data/avatar.jpeg");
 
 
 describe("posts", () => {
@@ -42,123 +44,22 @@ describe("posts", () => {
     });
 
     describe("given own auth header (creating post on own wall)", () => {
-      describe("given text content", () => {
-        it("returns 201 and post and text content", async () => {
-          const textContent = "foo bar";
+      const content = "this is the content";
+      givenNImgsAndTextContent(0, null);
+      
+      givenNImgsAndTextContent(0, content);
 
-          const { body, statusCode } = await requests(app)
-            .post(`/users/${user1.profilePath}/posts`)
-            .send({ content: textContent })
-            .set("Authorization", authHeader1);
-
-          expect(statusCode).toBe(201);
-          expect(body.onUser).toBe(user1.id);
-          expect(body.createdBy).toBe(user1.id);
-          expect(body.tinyPreview).toBe(null);
-          expect(body.content).toBe(textContent);
-          expect(body.postPath).toMatch(/foo/); // match content excluding the last word. Limited to 70 (may change) characters
-          expect(body.views).toBe(0);
-          expect(body.likes).toBe(0);
-          expect(body.dislikes).toBe(0);
-          expect(body.shares).toBe(0);
-          expect(body.comments).toEqual([]);
-          expect(body.imgs).toEqual([]);
-          expect(body.vids).toEqual([]);
-        })
-      })
-
-      describe("not given anything", () => {
-        it("returns 400 bad request", async () => {
-          const { body, statusCode } = await requests(app)
-            .post(`/users/${user1.profilePath}/posts`)
-            .set("Authorization", authHeader1);
-
-          expect(statusCode).toBe(400);
-          expect(body.message).toMatch(/no content/);
-        })
-      })
-
-      givenNImgsAndTextContent(1, "this is the content");
+      givenNImgsAndTextContent(1, content);
       givenNImgsAndTextContent(1, null);
 
-      givenNImgsAndTextContent(10, "this is the content");
+      givenNImgsAndTextContent(10, content);
       givenNImgsAndTextContent(10, null);
 
-      givenNImgsAndTextContent(11, "this is the content");
+      givenNImgsAndTextContent(11, content);
       givenNImgsAndTextContent(11, null);
     })
   })
 })
-
-
-function givenNImgsAndTextContent(imgsAmount: number, textContent: string | null) {
-  let describeContent = getDescribeContent();
-  let itContent = getItContent();
-
-  describe(describeContent, () => {
-    it(itContent, async () => {
-      const request = requests(app)
-        .post(`/users/${user1.profilePath}/posts`)
-        .set("Authorization", authHeader1);
-
-      if (textContent) request.field("content", textContent);
-
-      for (let i = 0; i < imgsAmount; i++) {
-        request.attach("imgs", jpegImgPath);
-      }
-
-      const { body, statusCode } = await request;
-      const isLimitExceeded = imgsAmount > imgsUploadLimit;
-
-      if (isLimitExceeded) {
-        expect(statusCode).toBe(400);
-        const expectRegex = `you've exceeded the limit of ${imgsUploadLimit} images per post`;
-        expect(body.message).toMatch(new RegExp(expectRegex));
-      } else {
-        expect(statusCode).toBe(201);
-        expectMetadataToBeZero(body);
-        expectPostIsOnCreatorsWall(body, user1);
-        if (textContent) {
-          expect(body.content).toBe(textContent);
-        }
-      }
-    })
-  })
-
-  function getDescribeContent() {
-    const wordEnding = imgsAmount > 1 ? "s" : "";
-    let content = `given ${imgsAmount} .jpeg image${wordEnding}`;
-    if (textContent) content += " and text content";
-
-    return content;
-  }
-
-  function getItContent() {
-    const wordEnding = imgsAmount > 1 ? "s" : "";
-    let content: string;
-
-    if (imgsAmount <= imgsUploadLimit) {
-      content = `returns 201 created and img${wordEnding} url${wordEnding}`;
-      if (textContent) content += " and textContent";
-    } else {
-      content = `returns 400 bad request because img limit of ${imgsUploadLimit} was exceeded`;
-    }
-
-    return content;
-  }
-}
-
-
-function expectPostIsOnCreatorsWall(body: any, user: HydratedDocument<IUser>) {
-  expect(body.onUser).toBe(user.id);
-  expect(body.createdBy).toBe(user.id);
-}
-
-
-function expectMetadataToBeZero(body: any) {
-  const { views, likes, dislikes, shares } = body;
-  expect([views, likes, dislikes, shares]).toEqual([0, 0, 0, 0]);
-}
 
 
 async function beforeAllCb() {
@@ -177,36 +78,4 @@ async function create2Users() {
 
 function getAuthHeadersFor2Users(user1: HydratedDocument<IUser>, user2: HydratedDocument<IUser>) {
   return [getAuthHeader(user1.id), getAuthHeader(user2.id)]
-}
-
-function given1JpegImgAndPossiblyNoTextContent(textContent: string | null) {
-  describe(`given 1 .jpeg image and ${textContent ? " " : "no"}text content`, () => {
-    it("returns 201 created and image url and tiny preview url", async () => {
-      const request = requests(app)
-        .post(`/users/${user1.profilePath}/posts`)
-        .attach("imgs", jpegImgPath)
-        .set("Authorization", authHeader1);
-        if (textContent) request.field("content", textContent);
-
-      const { body, statusCode } = await request;
-
-      expect(statusCode).toBe(201);
-      expect(body.onUser).toBe(user1.id);
-      expect(body.createdBy).toBe(user1.id);
-      expect(body.tinyPreview).toMatch(/https:\/\//);
-      expect(body.imgs.length).toBe(1);
-      expect(body.imgs[0].fullSize).toMatch(/https:\/\//);
-      expect(body.imgs[0].feedSize).toMatch(/https:\/\//);
-      expect(body.imgs[0].previewSize).toMatch(/https:\/\//);
-      if (textContent) {
-        expect(body.content).toBe(textContent);
-      } else {
-        expect(body.content).toBe(null);
-      }
-      expect(body.comments).toEqual([]);
-      expect(body.vids).toEqual([]);
-      const { views, likes, dislikes, shares } = body;
-      expect([views, likes, dislikes, shares]).toEqual([0, 0, 0, 0]);
-    })
-  })
 }
