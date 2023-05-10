def2a523182b3671c30a0d525d9992165cd6bf4b
Matvey <morozovmatvej75@gmail.com>
2023-05-10 19:23:39 +0300

    send 406 instead of 400 when profile path is unavaialbe

--- server/src/middleware/isProfilePathAvailable.ts
+++ server/src/middleware/isProfilePathAvailable.ts
@@ -1,15 +1,14 @@
 import { NextFunction } from "express";
 import User from "../models/User";
-import { BadRequestErr } from "../utils/errs";
+import { ConflictErr } from "../utils/errs";
 import { IReq, IRes } from "../utils/reqResInterfaces";
-import givenNImgsAndTextContent from "../__tests__/test_posts/givenNImgsAndTextContent/givenNImgsAndTextContent";
 
 export default async function checkProfilePathAvailable(req: IReq, res: IRes, next: NextFunction) {
   const { profilePath } = req.body;
 
   if (profilePath) {
     const userExists = !!(await User.findOne({ profilePath }));
-    if (userExists) throw new BadRequestErr("profile path unavailable");
+    if (userExists) throw new ConflictErr("profile path unavailable");
   }
 
   next();
