5d506e1bf4fe94f8a37f553dd43e0564874bad0a
Matvey <morozovmatvej75@gmail.com>
2023-05-17 17:58:13 +0300

    improve addComment() test helper

--- server/src/__tests__/test_comments/addComment.ts
+++ server/src/__tests__/test_comments/addComment.ts
@@ -1,12 +1,43 @@
 import requests from "supertest";
 import app from "../../app";
+import attachNFiles from "../utils/attachNImgs";
 import { postByUser1, user1AuthHeader } from "./comments.test";
 
-export default async function addComment(content?: string, replyTo?: string) {
-  const { statusCode, body } = await requests(app)
+
+interface IData {
+  content?: string,
+  replyTo?: string
+  media?: {
+    imgsAmount: number,
+    imgPath: string
+  },
+  authHeader?: string | null
+}
+
+
+export default async function addComment(data: IData) {
+  validatePassedData(data);
+
+  const { media: { imgPath, imgsAmount } = {}, content, replyTo } = data;
+  let authHeader = data.authHeader;
+  if (authHeader === undefined) authHeader = user1AuthHeader;
+  else if (authHeader === null) authHeader = "";
+
+  const request = requests(app)
     .post(`/posts/${postByUser1.postPath}/comments`)
     .send({ content, replyTo })
-    .set("Authorization", user1AuthHeader);
+    .set("Authorization", authHeader);
+  if (imgsAmount) attachNFiles("imgs", imgPath as string, imgsAmount, request);
 
-  return { statusCode, body };
+  return request;
+}
+
+function validatePassedData(data: IData) {
+  const { media: { imgPath, imgsAmount } = {} } = data;
+  if (
+    (imgPath !== undefined && imgsAmount === undefined) ||
+    (imgPath === undefined && imgsAmount !== undefined)
+  ) {
+    throw new Error("imgPath and imgsAmount must be either both defined or both undefined");
+  }
 }
