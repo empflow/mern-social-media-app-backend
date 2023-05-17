b576a561a064014d69bbafdd09109d0142c78347
Matvey <morozovmatvej75@gmail.com>
2023-05-17 13:19:09 +0300

    rename middleware & make more generic

---
+++ server/src/middleware/commentUploadImgsIfPresent.ts
@@ -0,0 +1,15 @@
+import { NextFunction } from "express";
+import optimizeAndUploadCommentImgs from "../utils/optimizeAndUploadCommentImgs";
+import optimizeAndUploadPostImgs from "../utils/optimizeAndUploadPostImgs";
+import { IReq, IRes } from "../utils/reqResInterfaces";
+
+export default async function commentUploadImgsIfPresent(req: IReq, res: IRes, next: NextFunction) {
+  if (!req.files || !req.files.length) return next();
+
+  const imgs = req.files;
+  const buffers = (imgs as Express.Multer.File[]).map(img => img.buffer);
+  const uploadResult = await optimizeAndUploadCommentImgs(buffers);
+
+  req.data.upload = uploadResult;
+  next();
+}
