8d2282b5d5ec72d4cc6a5807eb48298867955dde
Matvey <morozovmatvej75@gmail.com>
2023-05-17 11:48:24 +0300

    add doesArrHaveStrDuplicates() util

---
+++ server/src/utils/doesArrHaveStrDuplicates.ts
@@ -0,0 +1,11 @@
+export default function doesArrHaveStrDuplicates(arr: string[]) {
+  console.log("slkfdjs");
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (j === i) continue;
+      if (arr[i] === arr[j]) return true;
+    }
+  }
+
+  return false;
+}
