ccccece2b5741cdffb2a33fe7aa6b7a8f862b9cc
Matvey <morozovmatvej75@gmail.com>
2023-05-19 20:14:44 +0300

    improve checkReplyToCommentExists()

--- server/src/utils/checkReplyToCommentExists.ts
+++ server/src/utils/checkReplyToCommentExists.ts
@@ -1,12 +1,21 @@
-import { NextFunction } from "express";
 import Comment from "../models/Comment";
 import { NotFoundErr } from "./errs";
-import { IReq, IRes } from "./reqResInterfaces";
 
 
-export default async function checkReplyToCommentExists(replyTo: string | undefined) {
-  if (!replyTo) return;
+export default async function checkReplyToCommentExists(
+  replyTo: string | undefined, opts?: { shouldReturnBool: boolean }
+) {
+  const { shouldReturnBool = false } = opts ?? {};
   
+  if (shouldReturnBool && !replyTo) return false;
+  if (!replyTo) return;
+
   const comment = await Comment.findById(replyTo);
+  
+  if (shouldReturnBool) {
+    if (!replyTo) return false;
+    if (!comment) return false;
+    return true;
+  }
   if (!comment) throw new NotFoundErr("comment you're trying to reply to does not exist");
 }
