dd6a5336420107fb4adb6dde78c3791f7bfb960f
Matvey <morozovmatvej75@gmail.com>
2023-05-19 20:12:33 +0300

    improve expectCommentImgsUrlsMatchHttps()

--- server/src/__tests__/test_comments/expectCommentImgsUrlsMatchHttps.ts
+++ server/src/__tests__/test_comments/expectCommentImgsUrlsMatchHttps.ts
@@ -1,10 +1,7 @@
-interface IImgObj {
-  fullSize: string,
-  previewSize: string
-}
+import { ICommentImg } from "../../models/Comment";
 
 export default function expectCommentImgsUrlsMatchHttps(body: any) {
-  body.imgs.forEach((img: IImgObj) => {
+  body.imgs.forEach((img: ICommentImg) => {
     expect(img.fullSize).toMatch(/https:\/\//);
     expect(img.previewSize).toMatch(/https:\/\//);
   })
