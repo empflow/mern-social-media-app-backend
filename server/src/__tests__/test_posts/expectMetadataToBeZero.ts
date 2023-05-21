a82f6cdb0fdecdf31e5d1b71824a3ffa85a547f8
Matvey <morozovmatvej75@gmail.com>
2023-05-21 08:17:08 +0300

    add JSDoc to expectMetadataToBeZero() helper

--- server/src/__tests__/test_posts/expectMetadataToBeZero.ts
+++ server/src/__tests__/test_posts/expectMetadataToBeZero.ts
@@ -1,3 +1,7 @@
+/**
+ * expects that views, likes, dislikes and shares are set to 0
+ * @param body the body of the response
+ */
 export default function expectMetadataToBeZero(body: any) {
   const { views, likes, dislikes, shares } = body;
   expect([views, likes, dislikes, shares]).toEqual([0, 0, 0, 0]);
