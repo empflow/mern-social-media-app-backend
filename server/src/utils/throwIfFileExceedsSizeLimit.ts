a84db14a186ee5ec492d6ac6cf06a80cfd5c42a7
Matvey <morozovmatvej75@gmail.com>
2023-06-10 16:18:52 +0300

    improve throwIfFileSizeOverLimit

---
+++ server/src/utils/throwIfFileExceedsSizeLimit.ts
@@ -0,0 +1,21 @@
+import { BadRequestErr } from "./errs";
+
+
+export default function throwIfFileExceedsSizeLimit(
+  files: Buffer | Buffer[], limitInMb: number
+) {
+  if (Array.isArray(files)) {
+    files.forEach(file => checkAndThrowIfNeeded(file, limitInMb));
+  } else {
+    checkAndThrowIfNeeded(files, limitInMb);
+  }
+}
+
+
+function checkAndThrowIfNeeded(file: Buffer, limitInMb: number) {
+  const sizeInMb = file.byteLength / 1000000;
+  if (sizeInMb > limitInMb) {
+    const sizeInMbFixed = sizeInMb.toFixed(2);
+    throw new BadRequestErr(`File too large (${sizeInMbFixed}mb)`);
+  }
+}
