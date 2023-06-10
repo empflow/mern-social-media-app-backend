58980d848bd585cd5873354f8d28f175128724f2
Matvey <morozovmatvej75@gmail.com>
2023-06-10 18:52:44 +0300

    check if no new content provided on patch post

--- server/src/middleware/posts/patchPost/validator.ts
+++ server/src/middleware/posts/patchPost/validator.ts
@@ -6,13 +6,37 @@ import validateFileCount from "../../../utils/validateFileCount";
 import validateFilesToDeleteIds from "../../../utils/validateFilesToDeleteIds";
 
 export default async function patchPostValidator(req: IReq, res: IRes, next: NextFunction) {
+  throwIfNoNewContentProvided(req);
   const post = await getPostAndCheckItExists(req);
   validatePatchingOwnPost(req, post);
   validateFileCount(req, post);
   validateFilesToDeleteIds(req, post);
-
+  
+  req.data.content = getNewContent(req);
   req.data.post = post;
   next();
+}
+
+
+function throwIfNoNewContentProvided(req: IReq) {
+  const errMsg = "no content provided";
+  const { filesToDeleteIds, content } = req.body;
+
+  const noFiles = !req.files?.length;
+  if (noFiles && !filesToDeleteIds) {
+    if (typeof content === "string" && !content.trim()) {
+      throw new BadRequestErr(errMsg);
+    } else if (content === undefined) {
+      throw new BadRequestErr(errMsg);
+    }
+  }
+}
+
+
+function getNewContent(req: IReq) {
+  let { content } = req.body;
+  if (typeof content === "string") content = content.trim();
+  return content;
 }
 
 
