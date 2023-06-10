75a54ca124676c63c19b81f11b63f498201b925e
Matvey <morozovmatvej75@gmail.com>
2023-06-10 16:16:45 +0300

    add global config file

---
+++ server/src/config/global.ts
@@ -0,0 +1,6 @@
+export const imgSizeLimitInMb = 8;
+
+export const imgsAmountUploadLimit = 10;
+export const vidsAmountUploadLimit = 2;
+
+export const allowedFileExts = [".png", ".jpg", ".jpeg", ".webp"];
