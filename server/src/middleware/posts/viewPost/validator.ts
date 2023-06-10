efd8c1ca39a9c11c1fd390aee189db552f41974e
Matvey <morozovmatvej75@gmail.com>
2023-06-10 10:09:41 +0300

    parallelize requests inside viewPost controller

--- server/src/middleware/posts/viewPost/validator.ts
+++ server/src/middleware/posts/viewPost/validator.ts
@@ -1,5 +1,6 @@
 import { NextFunction } from "express";
 import { isValidObjectId } from "mongoose";
+import Post from "../../../models/Post";
 import PostView from "../../../models/PostView";
 import { BadRequestErr } from "../../../utils/errs";
 import { IReq, IRes } from "../../../utils/reqResInterfaces";
@@ -8,10 +9,19 @@ import { IReq, IRes } from "../../../utils/reqResInterfaces";
 export default async function viewPostValidator(req: IReq, res: IRes, next: NextFunction) {
   const { postId } = req.params;
   const { userId } = req.data.user;
-
   if (!isValidObjectId(postId)) throw new BadRequestErr("invalid post id");
-  const alreadyViewed = await PostView.findOne({ userId, postId });
+
+  const [postView, post] = await getPostViewAndPost(postId, userId);
+  const alreadyViewed = !!postView;
   if (alreadyViewed) throw new BadRequestErr("post already viewed");
 
+  req.data.post = post;
   next();
+}
+
+
+async function getPostViewAndPost(postId: string, userId: string) {
+  const postViewPromise = PostView.findOne({ userId, postId });
+  const postPromise = Post.findById(postId, { views: 1 });
+  return Promise.all([postViewPromise, postPromise]);
 }
